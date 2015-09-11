37b6a30dbfdbb0fcac72310772d44905a6fc57f8
fredghostkyle <fredghostkyle@yahoo.com>
2015-09-11 11:32:23 -0400

    Can challenge.

---
+++ src/edu/kyleknobloch/APCS/Classwork/September/Can.java
@@ -0,0 +1,75 @@
+
+/**
+ * Created by Kyle Knobloch on 9/11/15,
+ * AP CS
+ * Mr. Drugan
+ */
+
+
+public class Can {
+
+    private String name;
+    private String contents;
+    private double volume;
+    private double cost;
+
+    public Can() {
+        //default constructor
+
+        name     = "Diet Doctor Pepper";
+        contents = "Diet Doctor Pepper";
+        volume   = 11.5;
+        cost     = 1.29;
+
+        toString();
+
+    }
+
+    public Can(String n, String c, double v, double cost) {
+        //Set evreything
+
+        name      = n;
+        contents  = c;
+        volume    = v;
+        this.cost = cost;
+
+        toString();
+    }
+
+    public String toString() {
+        //public toString
+
+        String msg = "Name: " + getName() + ".\nContents: " + getContents() + ".\nVolume: " + getVolume() + ".\nCost: $" + getCost();
+
+
+        System.out.println(msg);
+        return msg;
+
+    }
+
+
+    /*
+     * Get methods
+     */
+
+    public String getContents() {
+
+        return contents;
+    }
+
+    public double getVolume() {
+
+        return volume;
+    }
+
+    public double getCost() {
+
+        return cost;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+}
---
+++ src/edu/kyleknobloch/APCS/Classwork/September/CanTester.java
@@ -0,0 +1,19 @@
+
+/**
+ * Created by Kyle Knobloch on 9/11/15,
+ * AP CS
+ * Mr. Drugan
+ */
+
+
+public class CanTester {
+
+    public static void main (String args[]) {
+        //Main class
+
+        Can canDrPepper = new Can();
+        System.out.println();
+        Can canDrugan   = new Can("Diet Drugan", "Diet Drugan", 11.5, 1000); //set everything constructor
+
+    }
+}
