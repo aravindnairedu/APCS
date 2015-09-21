83b3fead304524411ce423c2764f6849ab512044
fredghostkyle <fredghostkyle@yahoo.com>
2015-09-21 08:27:27 -0400

    Fixed the tester

--- src/edu/kyleknobloch/APCS/Classwork/September/TestScoresTester.java
+++ src/edu/kyleknobloch/APCS/Classwork/September/TestScoresTester.java
@@ -7,26 +7,19 @@ public class TestScoresTester {
     public static void main (String args[]) {
 
         double grade1, grade2, grade3;
-
         Scanner console = new Scanner(System.in);
-        TestScores test = new TestScores();
 
 
         System.out.println("Grade 1: ");
         grade1 = console.nextDouble();
-        System.out.println("Letter grade is: " + test.findLetterGrade(grade1));
         System.out.println("Grade 2: ");
         grade2 = console.nextDouble();
-        System.out.println("Letter grade is: " + test.findLetterGrade(grade2));
         System.out.println("Grade 3: ");
         grade3 = console.nextDouble();
-        System.out.println("Letter grade is: " + test.findLetterGrade(grade3));
 
 
         TestScores avg = new TestScores(grade1, grade2, grade3);
 
-
-
-
+        System.out.println(avg.toString());
     }
 }
