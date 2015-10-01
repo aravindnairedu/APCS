1e104e4feab7371f070ab3b10b3dc6053569757c
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-01 10:57:59 -0400

    Learning arrays.

---
+++ src/edu/kyleknobloch/APCS/Classwork/September/MessyArray.java
@@ -0,0 +1,20 @@
+package edu.kyleknobloch.APCS.Classwork.September;
+
+
+public class MessyArray {
+
+    public static void main (String[] args) {
+
+        int[] testArray = new int[5];
+
+        testArray[0] = 6;
+        testArray[1] = 4;
+        testArray[2] = 4;
+        testArray[3] = 4;
+        testArray[4] = 6;
+
+
+
+
+    }
+}
