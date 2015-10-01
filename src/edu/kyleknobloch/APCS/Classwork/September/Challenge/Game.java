c21aa12da72e84266414a6be2224eb5f1f55dbbd
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-01 11:24:42 -0400

    Made check() so more efficient

--- src/edu/kyleknobloch/APCS/Classwork/September/Challenge/Game.java
+++ src/edu/kyleknobloch/APCS/Classwork/September/Challenge/Game.java
@@ -7,13 +7,13 @@ public class Game {
     private int score;
     private int count1, count2, count3, count4, count5, count6;
     private boolean usedChance = false;
+    private String message;
+    private boolean scored = false;
 
     public Game() {
 
         int dice1, dice2, dice3, dice4, dice5, sum, turnsLeft;
         int turn = 0;
-        boolean scored = false;
-        String message = "";
         JOptionPane JO = new JOptionPane();
 
 
@@ -32,37 +32,8 @@ public class Game {
             eval(dice5);
 
 
-            //JO.showMessageDialog(null, "Your dice are: \n" + dice1 + "\n" + dice2 + "\n" + dice3 + "\n" + dice4 + "\n" + dice5 + "\n\nCalculating your score for this turn now!");
-            
-
-            //Check for a straight
-            if((count1 == 1 && count2 == 1 && count3 == 1 && count4 == 1 && count5 == 1)||(count2 == 1 && count3 == 1 && count4 == 1 && count5 == 1 && count6 == 1)){
-                addScore(40);
-                message = "You got a straight! Add 40 points.";
-                scored = true;
-            }
-            
-            //Check for Full house
-            if((count1 == 3 || count2 == 3 || count3 == 3 || count4 == 3 || count5 == 3 || count6 == 3 ) && (count1 == 2 || count2 == 2 || count3 == 2 || count4 == 2 || count5 == 2 || count6 == 2)){
-                addScore(35);
-                message = "You got a Full house! Add 35 points ";
-                scored = true;
-            }
-            
-            //Check for YAHTEEZ!
-            if(count1 == 5 || count2 == 5 || count3 == 5 || count4 == 5 || count5 == 5 || count6 == 5){
-                message = "YAHTEEZ!!!!!!!!!";
-                addScore(50);
-                scored = true;
-            }
-
-            
-            //Check for 4 of a kind
-            if(count1 == 4 || count2 == 4 || count3 == 4 || count4 == 4 || count5 == 4 || count6 == 4){
-                addScore(30);
-                message = "You got 4 of a kind!";
-                scored = true;
-            }
+            //run the checks
+            check();
 
             //Run some calculations and background items that need to be ran before we display the final message.
             turn++;
@@ -100,7 +71,40 @@ public class Game {
         JO.showMessageDialog(null, "This round has ended! Your final score was " + getScore() + "! Hope to see you again.");
     }
 
-    public void eval(int dice) {
+    private void check() {
+
+        //Check for a straight
+        if((count1 == 1 && count2 == 1 && count3 == 1 && count4 == 1 && count5 == 1)||(count2 == 1 && count3 == 1 && count4 == 1 && count5 == 1 && count6 == 1)){
+            addScore(40);
+            message = "You got a straight! Add 40 points.";
+            scored = true;
+        }
+
+        //Check for Full house
+        if((count1 == 3 || count2 == 3 || count3 == 3 || count4 == 3 || count5 == 3 || count6 == 3 ) && (count1 == 2 || count2 == 2 || count3 == 2 || count4 == 2 || count5 == 2 || count6 == 2)){
+            addScore(35);
+            message = "You got a Full house! Add 35 points ";
+            scored = true;
+        }
+
+        //Check for YAHTEEZ!
+        if(count1 == 5 || count2 == 5 || count3 == 5 || count4 == 5 || count5 == 5 || count6 == 5){
+            message = "YAHTEEZ!!!!!!!!!";
+            addScore(50);
+            scored = true;
+        }
+
+
+        //Check for 4 of a kind
+        if(count1 == 4 || count2 == 4 || count3 == 4 || count4 == 4 || count5 == 4 || count6 == 4){
+            addScore(30);
+            message = "You got 4 of a kind!";
+            scored = true;
+        }
+
+    }
+
+    private void eval(int dice) {
         switch (dice) {
             case 1:
                 count1++;
