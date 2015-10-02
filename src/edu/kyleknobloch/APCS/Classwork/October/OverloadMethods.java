32ea839ddf07abe51d3fa9bf4661ee3d547b9c8c
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-02 14:40:43 -0400

    More Imutable MUAHAHAHHA

--- src/edu/kyleknobloch/APCS/Classwork/October/OverloadMethods.java
+++ src/edu/kyleknobloch/APCS/Classwork/October/OverloadMethods.java
@@ -9,44 +9,45 @@ public class OverloadMethods {
         double elevin = 11;
         short sTen = 10;
         short sElevin = 11;
+        OverloadMethods OLM = new OverloadMethods();
 
 
-        System.out.println(add(10, 11));
-        System.out.println(add(ten, elevin));
-        System.out.println(add(10f, 11f));
-        System.out.println(add(10l, 11l));
-        System.out.println(add(sTen, sElevin));
-        System.out.println(add("ten", "elevin"));
+        System.out.println(OLM.add(10, 11));
+        System.out.println(OLM.add(ten, elevin));
+        System.out.println(OLM.add(10f, 11f));
+        System.out.println(OLM.add(10l, 11l));
+        System.out.println(OLM.add(sTen, sElevin));
+        System.out.println(OLM.add("ten", "elevin"));
 
     }
 
-    public static int add(int one, int two) {
+    private int add(int one, int two) {
         System.out.println("Integer");
         return one + two;
     }
 
-    public static double add(double one, double two) {
+    private double add(double one, double two) {
         System.out.println("Double");
         return one + two;
     }
 
-    public static float add(float one, float two) {
+    private float add(float one, float two) {
         System.out.println("Float");
         return one + two;
     }
 
-    public static long add(long one, long two) {
+    private long add(long one, long two) {
         System.out.println("Long");
         return one + two;
     }
 
-    public static short add(short one, short two) {
+    private short add(short one, short two) {
         System.out.println("Short");
         int sum = one + two;
         return (short) sum;
     }
 
-    public static String add(String one, String two) {
+    private String add(String one, String two) {
         System.out.println("String");
         return one + " " + two;
     }
