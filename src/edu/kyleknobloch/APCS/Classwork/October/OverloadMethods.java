e59a4394b5ef24daedcc6fc7e213f3533d84cd4c
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-02 14:17:16 -0400

    OverloadMethods Notes & MessyArray testing

--- src/edu/kyleknobloch/APCS/Classwork/October/OverloadMethods.java
+++ src/edu/kyleknobloch/APCS/Classwork/October/OverloadMethods.java
@@ -5,12 +5,50 @@ public class OverloadMethods {
 
     public static void main (String[] args) {
 
+        double ten = 10;
+        double elevin = 11;
+        short sTen = 10;
+        short sElevin = 11;
+
+
+        System.out.println(add(10, 11));
+        System.out.println(add(ten, elevin));
+        System.out.println(add(10f, 11f));
+        System.out.println(add(10l, 11l));
+        System.out.println(add(sTen, sElevin));
+        System.out.println(add("ten", "elevin"));
 
     }
 
-    public int add(int one, int two) {
-
+    public static int add(int one, int two) {
+        System.out.println("Integer");
+        return one + two;
     }
 
-    public double add
+    public static double add(double one, double two) {
+        System.out.println("Double");
+        return one + two;
+    }
+
+    public static float add(float one, float two) {
+        System.out.println("Float");
+        return one + two;
+    }
+
+    public static long add(long one, long two) {
+        System.out.println("Long");
+        return one + two;
+    }
+
+    public static short add(short one, short two) {
+        System.out.println("Short");
+        int sum = one + two;
+        return (short) sum;
+    }
+
+    public static String add(String one, String two) {
+        System.out.println("String");
+        return one + " " + two;
+    }
+
 }
--- src/edu/kyleknobloch/APCS/Classwork/September/MessyArray.java
+++ src/edu/kyleknobloch/APCS/Classwork/September/MessyArray.java
@@ -3,18 +3,28 @@ package edu.kyleknobloch.APCS.Classwork.September;
 
 public class MessyArray {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws ArrayIndexOutOfBoundsException {
 
-        int[] testArray = new int[5];
-
-        testArray[0] = 6;
-        testArray[1] = 4;
-        testArray[2] = 4;
-        testArray[3] = 4;
-        testArray[4] = 6;
+        int[] testArray = new int[] {6,6,6,4,4};
 
 
+        testArray[4] = 4;
 
+        //Arrays are usefull for printing out large ammounts of datas very easly, as shown below.
+
+        try {
+            System.out.println(testArray.length);
+
+            for (int i = 0; i < 6; i++) {
+                System.out.println(testArray[i]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException: " + e.getMessage());
+        }
+
+
+        System.out.println(testArray.toString());
 
     }
 }
