b12ea6ed136ae40e61c4b18375b12ea01f84e944
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-07 12:34:43 -0400

    Revert "Class I missed update."
    This reverts commit 00c5b810a6f268841b8c6ea0ebe35f5a4b4aa895.

--- src/edu/kyleknobloch/APCS/Classwork/October/ChoHan/Dealer.java
+++ src/edu/kyleknobloch/APCS/Classwork/October/ChoHan/Dealer.java
@@ -2,100 +2,82 @@ package edu.kyleknobloch.APCS.Classwork.October.ChoHan;
 /**
    Dealer class for the game of ChoHan
 */
-
+   
 public class Dealer
 {
    private int die1Value;  // The value of die #1
    private int die2Value;  // The value of die #2
-   private int die3Value;  // The value of die #3
-   private int die4Value;  // The value of die #4
-
+   
    /**
-    Constructor
-    */
-
+      Constructor
+   */
+   
    public Dealer()
    {
       die1Value = 0;
       die2Value = 0;
-      die3Value = 0;
-      die4Value = 0;
    }
-
+   
    /**
-    The rollDice method rolls the dice and saves
-    their values.
-    */
-
+      The rollDice method rolls the dice and saves
+      their values.
+   */
+   
    public void rollDice()
    {
       final int SIDES = 6; // Number of sides for the dice
-
+      
       // Create the two dice. (This also rolls them.)
-      ChoHanDie die1 = new ChoHanDie(SIDES);
-      ChoHanDie die2 = new ChoHanDie(SIDES);
-      ChoHanDie die3 = new ChoHanDie(SIDES);
-      ChoHanDie die4 = new ChoHanDie(SIDES);
-
+      ChoHanDie choHanDie1 = new ChoHanDie(SIDES);
+      ChoHanDie choHanDie2 = new ChoHanDie(SIDES);
+      
       // Record their values.
-      die1Value = die1.getValue();
-      die2Value = die2.getValue();
-      die3Value = die3.getValue();
-      die4Value = die4.getValue();
+      die1Value = choHanDie1.getValue();
+      die2Value = choHanDie2.getValue();
    }
-
+   
    /**
-    The getChoOrHan method returns the result of
-    the dice roll, Cho or Han.
-    @return Either "Cho (even)" or "Han (odd)"
-    */
-
+      The getChoOrHan method returns the result of
+      the dice roll, Cho or Han.
+      @return Either "Cho (even)" or "Han (odd)"
+   */
+   
    public String getChoOrHan()
    {
       String result; // To hold the result
-
+      
       // Get the sum of the dice.
-      int sum = die1Value + die2Value + die3Value + die4Value;
-
+      int sum = die1Value + die2Value;
+      
       // Determine even or odd.
       if (sum % 2 == 0)
          result = "Cho (even)";
       else
          result = "Han (odd)";
-
+      
       // Return the result.
       return result;
    }
-
+   
    /**
-    The getDie1Value method returns the value of
-    die #1.
-    @return The die1Value field
-    */
-
-   public int getDieValue(int dieNumber)
+      The getDie1Value method returns the value of
+      die #1.
+      @return The die1Value field
+   */
+   
+   public int getDie1Value()
    {
-      int value = 0;
-
-      switch ( dieNumber )
-      {
-         case 1:
-            value = die1Value;
-            break;
-         case 2:
-            value = die2Value;
-            break;
-         case 3:
-            value = die3Value;
-            break;
-         case 4:
-            value = die4Value;
-            break;
-         default:
-            value = 0;
-      }
-
-      return value;
+      return die1Value;
    }
 
+   /**
+      The getDie2Value method returns the value of
+      die #2.
+      @return The die2Value field
+   */
+   
+   public int getDie2Value()
+   {
+      return die2Value;
+   }
 }
