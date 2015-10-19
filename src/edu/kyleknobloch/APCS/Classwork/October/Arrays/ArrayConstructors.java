d4d688a889dfd9e6ae63f833685bfdc2cc0def75
fredghostkyle <fredghostkyle@yahoo.com>
2015-10-19 12:49:50 -0400

    More array work.

--- src/edu/kyleknobloch/APCS/Classwork/October/Arrays/ArrayConstructors.java
+++ src/edu/kyleknobloch/APCS/Classwork/October/Arrays/ArrayConstructors.java
@@ -5,36 +5,50 @@ import java.util.Random;
 
 public class ArrayConstructors {
 
+    private int arrays[] = new int[100];
+    boolean booleans[] = new boolean[100];
+
+    /**
+     * Default constructor
+     * @PRE  none
+     * @POST none
+     */
     public ArrayConstructors () {
-        arrayAllOne();
-        arrayRandomNumbers();
-        arrayBooleon();
+        //I do nothing, trust me I'm a default constructor
     }
 
-
+    /**
+     * @PRE  That the array arrays has ben created
+     * @POST none
+     */
     public void arrayAllOne () {
 
-        int allOnes[] = new int[100];
-
         for (int i = 0; i <= 99; i++) {
-            allOnes[i] = -1;
-            System.out.println(i + ": " + allOnes[i]);
+            arrays[i] = -1;
+            //System.out.println(i + ": " + array[i]);
         }
 
     }
 
+    /**
+     * @PRE  That the array arrays has ben created
+     * @POST set array arrays to random numbers between 0-999.
+     */
     public void arrayRandomNumbers () {
-        int randomNumbers[] = new int[100];
 
         for (int i = 0; i <= 99; i++) {
-            randomNumbers[i] = random(1000);
-            System.out.println(i + ": " + randomNumbers[i]);
+            arrays[i] = random(1000);
+            //System.out.println(i + ": " + array[i]);
         }
 
     }
 
+
+    /**
+     * @PRE  That the booleans arrays has ben created
+     * @POST Randomly sets array booleons to true or false.
+     */
     public void arrayBooleon () {
-        boolean booleans[] = new boolean[100];
 
         for (int i = 0; i <= 99; i++) {
             int n = random(2);
@@ -43,11 +57,48 @@ public class ArrayConstructors {
             } else if (n == 1) {
                 booleans[i] = false;
             }
-            System.out.println(i + ": " + booleans[i]);
+            //System.out.println(i + ": " + booleans[i]);
         }
 
     }
 
+    /**
+     * @PRE  That the array arrays has values set, call arrayRandomNumbers or arrayAllOne
+     * @POST @return
+     * @return String message, prints the array block and it's contents (a number)
+     */
+    @Override
+    public String toString() {
+
+        String message = "";
+        for (int i = 0; i <= 99; i++) {
+            message = message + "\n" + i + ": " + arrays[i];
+        }
+
+        return message;
+    }
+
+    /**
+     * @PRE  That the array booleans has values set, call arrayBooleon
+     * @POST @return
+     * @return String message, prints the array block and it's contents (true/false)
+     */
+    public String toStringBoolean() {
+
+        String message = "";
+        for (int i = 0; i <= 99; i++) {
+            message = message + "\n" + i + ": " + booleans[i];
+        }
+
+        return message;
+    }
+
+    /**
+     * @PRE  @param
+     * @POST @return
+     * @param n how high is the random number generator will create numbers
+     * @return a number from the random number generator
+     */
     public int random (int n) {
         Random r = new Random();
         return r.nextInt(n);
--- src/edu/kyleknobloch/APCS/Classwork/October/Arrays/ArrayTester.java
+++ src/edu/kyleknobloch/APCS/Classwork/October/Arrays/ArrayTester.java
@@ -1,4 +1,19 @@
 package edu.kyleknobloch.APCS.Classwork.October.Arrays;
+
+
 public class ArrayTester {
-    public static void main (String args[]) { ArrayConstructors ac = new ArrayConstructors(); }
+
+    public static void main (String args[]) {
+        //This is the main in the tester. I am not wrighting PRE and POST conditions for this. Come on Mr. Drugan
+
+        ArrayConstructors ac = new ArrayConstructors();
+
+        ac.arrayAllOne();
+        System.out.println(ac.toString());
+        ac.arrayRandomNumbers();
+        System.out.println(ac.toString());
+        ac.arrayBooleon();
+        System.out.println(ac.toStringBoolean());
+
+    }
 }
