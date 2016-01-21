0ec73d0595bce99aeeef09a5273f3a53f3a8d100
fredghostkyle <fredghostkyle@yahoo.com>
2016-01-21 10:41:21 -0500

    Part one of Question 1.

---
+++ src/edu/kyleknobloch/APCS/Tests/midterm/Question1/BoysAndGirls.java
@@ -0,0 +1,96 @@
+package edu.kyleknobloch.APCS.Tests.midterm.Question1;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Kyle Knobloch
+ * AP Computer Science Midterm Exam
+ *
+ * Question 1:
+ * 35pt
+ */
+
+public class BoysAndGirls {
+
+    private ArrayList<String> boys = new ArrayList<>();
+    private ArrayList<String> girls = new ArrayList<>();
+
+
+    public BoysAndGirls() {
+        readArrays(); //Fill the array lists
+
+    }
+
+    private void readArrays() {
+        String boysFile = "BoyNames.txt";
+        String girlsFile = "GirlNames.txt";
+
+        /* boys */
+        try{
+            // Open the file.
+            File boyFILE = new File(boysFile);
+            Scanner theBoy = new Scanner(boyFILE);
+
+            File girlFILE = new File(girlsFile);
+            Scanner theGirl = new Scanner(girlFILE);
+
+            /**
+             * boys
+             */
+            while (theBoy.hasNext())
+            {
+                boys.add(theBoy.next());
+                System.out.print("boy"); //DEBUG
+            }
+
+            /**
+             * girls
+             */
+            while (theGirl.hasNext())
+            {
+                girls.add(theGirl.next());
+                System.out.print("girl"); //DEBUG
+            }
+
+            // Close the file.
+            theBoy.close();
+            theGirl.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            // Thrown by the Scanner constructor when
+            // the file is not found.
+            JOptionPane.showMessageDialog(null, "The file " + boysFile + " does not exist.");
+        }
+        catch(InputMismatchException e)
+        {
+            // Thrown by the Scanner class's nextDouble
+            // method when a non-numeric value is found.
+            JOptionPane.showMessageDialog(null, "Non-numeric data found in the file.");
+        }
+        catch (Exception e) {
+            //loo
+            JOptionPane.showMessageDialog(null, "Some crazy shit went down! Lets try that again... " + e);
+        }
+
+        System.out.println("it is done.");
+    }
+
+    public String search (String name) {
+        int count = 0;
+
+
+
+
+
+
+        return "The name \"" + name + "\" Appeared " + count + "times.";
+    }
+
+
+}
---
+++ src/edu/kyleknobloch/APCS/Tests/midterm/Question1/Names.java
@@ -0,0 +1,24 @@
+package edu.kyleknobloch.APCS.Tests.midterm.Question1;
+
+/**
+ * Kyle Knobloch
+ * AP Computer Science Midterm Exam
+ *
+ * Question 1:
+ * 35pt
+ *
+ * Boys and Girls Names
+ *
+ *
+ * THIS IS THE TESTER CLASS
+ *
+ */
+public class Names {
+
+    public static void main (String args[]) {
+        BoysAndGirls names = new BoysAndGirls();
+
+    }
+
+
+}
