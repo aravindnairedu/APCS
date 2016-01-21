8b8ba1e73e9c89218aa339506ccd6ccda4b2137a
fredghostkyle <fredghostkyle@yahoo.com>
2016-01-21 10:21:31 -0500

    init midterm

---
+++ src/edu/kyleknobloch/APCS/Tests/midterm/QuestionOne.java
@@ -0,0 +1,10 @@
+package edu.kyleknobloch.APCS.Tests.midterm;
+
+/**
+ * Created by kyleknobloch on 1/21/16,
+ * For
+ * *
+ * Actions:
+ */
+public class QuestionOne {
+}
---
+++ src/edu/kyleknobloch/APCS/Tests/midterm/QuestionThree.java
@@ -0,0 +1,12 @@
+package edu.kyleknobloch.APCS.Tests.midterm;
+
+/**
+ * Kyle Knobloch
+ * AP Computer Sicnece Midterm Exam
+ *
+ * Question 3:
+ * 35pt, 5 extra credit
+ *
+ */
+public class QuestionThree {
+}
