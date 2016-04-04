54f26cbdf72d9ac9f3e9e3181fc1b1607a714244
fredghostkyle <fredghostkyle@yahoo.com>
2016-04-04 10:51:58 -0400

    less french words

--- src/edu/kyleknobloch/APCS/Classwork/March/RecursionTest.java
+++ src/edu/kyleknobloch/APCS/Classwork/March/RecursionTest.java
@@ -1,24 +1,31 @@
 package edu.kyleknobloch.APCS.Classwork.March;
 
 /**
- * Created by kyleknobloch on 3/30/16,
- * For
- * *
- * Actions:
+ *
  */
 public class RecursionTest {
 
-    public static void main (String args[]) {
-        System.out.print(myPower(1, 3, 3));
+    public static void main(String args[]) {
+        System.out.print(myPower(1, 3, 3) + "\n");
+
+        showMe(0);
     }
 
-    public static int myPower (int init, int base, int power) {
 
+    public static int myPower(int element, int base, int power) {
         if (power == 0)
-            return init;
+            return element;
         else
-            return myPower(init * base, base, power -1);
+            return myPower(element * base, base, power - 1);
+    }
 
+
+    public static void showMe(int arg) {
+        if (arg < 10)
+            showMe(arg + 1);
+        else
+            System.out.print(arg);
     }
 
 }
+
